fa62f3810fabed665a1a63bc7c47041934461f48
Wnagfeng <2712442320@qq.com>
2024-09-07 13:26:48 +0800

    :fire: HelloWrod

---
+++ 02Hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+/*
+cargo run   -- 这是运行命令
+cargo build -- 编译命令
+cargo check -- 仅检查语法 
+cargo test  -- 运行测试用例
+cargo new hello_cargo  -- 创建新项目
+ */
+fn main() {
+    println!("Hello, world!");
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# 学习Rust
+
+# Day1
+
+### 相关命令：
+
+```rust
+cargo run   -- 这是运行命令
+cargo build -- 编译命令
+cargo check -- 仅检查语法 
+cargo test  -- 运行测试用例
+cargo new hello_cargo  -- 创建新项目
+```
+
+
+
